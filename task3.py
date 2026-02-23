52a9eb88052e9b1bdf984ce95ff8c348f29e4f7f
leoalfaro868-a11y <leo.alfaro868@students.cmcss.net>
2026-02-23 12:01:05 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,43 @@
+from data import a, b, c, d, e, f
+
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+
+print(type(a))
+a = float(a)
+print(a)
+
+# b
+
+print(type(b))
+b = str(b)
+print(b)
+
+# c
+
+print(type(c))
+c = int(c)
+print(c)
+
+# d
+
+print(type(d))
+d = int(d)
+print(d)
+
+# e
+
+print(type(e))
+e = str(e)
+print(e)
+
+# f
+
+print(type(f))
+f = float(f)
+print(f)
+
 
